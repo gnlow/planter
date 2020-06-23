2a6eeb50424ccb862c90c1abb17539806c409bff
Gnlow <entrydark@gmail.com>
2020-06-23 12:35:50 +0900

    use infiniteArray module

--- mod.ts
+++ mod.ts
@@ -1,26 +1,9 @@
 import {xxHash32} from "https://raw.githubusercontent.com/gnlow/deno-xxhash/master/mod.ts"
+import infiniteArray from "https://raw.githubusercontent.com/gnlow/infinite-array/master/mod.ts"
 
 const hash = (seed: number, index: number) => 
     xxHash32(`${String(seed)} ${index}`) / 0xffffffff
 
-export default (seed: number) => 
-    new Proxy({
-        *[Symbol.iterator](){
-            let index = 0
-            while(true){
-                yield hash(seed, index++)
-            }
-        }
-    } as {
-        [index: number]: number
-        [Symbol.iterator]: GeneratorFunction
-    }, {
-        get: (target, index: any) => {
-            if(index in target){
-                return target[index]
-            }
-            if(!Number.isNaN(Number(index))) {
-                return hash(seed, index)
-            }
-        }
-    })
+export default (seed: number) => infiniteArray(
+        (index: number) => hash(seed, index)
+    )
