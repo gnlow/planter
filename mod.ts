a3b9f73bf76d00e9056c1f92a0ace7be5022d00e
Gnlow <entrydark@gmail.com>
2020-07-19 21:07:24 +0900

    export hash

--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 import {xxHash32} from "https://raw.githubusercontent.com/gnlow/deno-xxhash/master/mod.ts"
 import infiniteArray from "https://raw.githubusercontent.com/gnlow/infinite-array/master/mod.ts"
 
-const hash = (seed: number, index: number) => 
-    xxHash32(`${String(seed)} ${index}`) / 0xffffffff
+export const hash = (seed: number, key: string) => 
+    xxHash32(`${String(seed)} ${key}`) / 0xffffffff
 
 export default (seed: number) => infiniteArray(
-        (index: number) => hash(seed, index)
+        (index: number) => hash(seed, String(index))
     )
